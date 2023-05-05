eb41a3e11016a9b5f010245a22469b2dd0dbc11f
maddernd <maddernd@deakin.edu.au>
2023-05-06 06:57:24 +1000

    Working Backend + Frontend

--- frontend/numbas-test-fe/src/app/components/numbas-component/numbas-component.component.ts
+++ frontend/numbas-test-fe/src/app/components/numbas-component/numbas-component.component.ts
@@ -1,31 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NumbasService } from '../../services/numbas.service';
 
 @Component({
   selector: 'app-numbas',
   templateUrl: './numbas-component.component.html'
 })
-export class NumbasComponent {
+export class NumbasComponent implements OnInit {
   constructor(private numbasService: NumbasService) {}
 
+  ngOnInit(): void {
+    this.interceptIframeRequests();
+  }
+
   launchNumbasTest(): void {
-    this.numbasService.getNumbasTest().subscribe(
-      (blobData) => {
-        // Load the HTML data into an iframe
-        const iframe = document.createElement('iframe');
-        const reader = new FileReader();
-        reader.addEventListener('loadend', () => {
-          iframe.srcdoc = reader.result as string;
-          iframe.style.width = '100%';
-          iframe.style.height = '800px';
-          document.body.appendChild(iframe);
-        });
-        reader.readAsText(blobData);
-      },
-      (error) => {
-        console.error('Error fetching Numbas test:', error);
+    const iframe = document.createElement('iframe');
+    iframe.src = 'http://localhost:3000/api/numbas_api/index.html';
+    iframe.style.width = '100%';
+    iframe.style.height = '800px';
+    document.body.appendChild(iframe);
+  }
+  
+
+  interceptIframeRequests(): void {
+    const originalOpen = XMLHttpRequest.prototype.open;
+    const numbasService = this.numbasService;
+    XMLHttpRequest.prototype.open = function (this: XMLHttpRequest, method: string, url: string | URL, async: boolean = true, username?: string | null, password?: string | null) {
+      if (typeof url === 'string' && url.startsWith('/api/numbas_api/')) {
+        const resourcePath = url.replace('/api/numbas_api/', '');
+        this.abort();
+        numbasService.fetchResource(resourcePath).subscribe(
+          (resourceData) => {
+            if (this.onload) {
+              this.onload.call(this, resourceData);
+            }
+          },
+          (error) => {
+            console.error('Error fetching Numbas resource:', error);
+          }
+        );
+      } else {
+        originalOpen.call(this, method, url, async, username, password);
       }
-    );
+    };
   }
   
 }
--- frontend/numbas-test-fe/src/app/services/numbas.service.ts
+++ frontend/numbas-test-fe/src/app/services/numbas.service.ts
@@ -1,24 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NumbasService {
-  private readonly API_URL = 'http://localhost:3000/api/numbas_api/index.html'; 
+  private readonly API_URL = 'http://localhost:3000/api/numbas_api';
 
   constructor(private http: HttpClient) {}
 
-  getNumbasTest(): Observable<any> {
-    return this.http.get(`${this.API_URL}`, { responseType: 'text' }).pipe(
+  fetchResource(resourcePath: string): Observable<any> {
+    const resourceUrl = `${this.API_URL}/${resourcePath}`;
+    const resourceMimeType = this.getMimeType(resourcePath);
+
+    return this.http.get(resourceUrl, { responseType: 'blob' }).pipe(
+      map((blob) => new Blob([blob], { type: resourceMimeType })),
       catchError((error: HttpErrorResponse) => {
-        console.error('Error fetching Numbas test:', error);
-        return throwError('Error fetching Numbas test.');
+        console.error('Error fetching Numbas resource:', error);
+        return throwError('Error fetching Numbas resource.');
       })
     );
   }
-  
-  
-  
+
+  getMimeType(resourcePath: string): string {
+    const extension = resourcePath.split('.').pop()?.toLowerCase();
+    const mimeTypeMap: { [key: string]: string } = {
+      'html': 'text/html',
+      'css': 'text/css',
+      'js': 'application/javascript',
+      'json': 'application/json',
+      'png': 'image/png',
+      'jpg': 'image/jpeg',
+      'gif': 'image/gif',
+      'svg': 'image/svg+xml'
+      // Add any other needed MIME types here
+    };
+
+    return mimeTypeMap[extension || ''] || 'text/plain';
+  }
 }
